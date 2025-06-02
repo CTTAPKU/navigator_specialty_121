c430e7a3e3da48e31dedfc6ab11ca58716904d6c
CTTAPKU <cttarkug@gmail.com>
2025-06-02 15:15:20 +0300

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:diploma/pages/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarIconBrightness: Brightness.dark,
   ));
